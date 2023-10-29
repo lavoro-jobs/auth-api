2e5057a3e7d482de27d229962102db1724ed323d
Marko Bolt <marko.bolt@fer.hr>
2023-10-29 23:10:14 +0100

    separated helper functions from routers

--- lavoro_auth_api/database/queries.py
+++ lavoro_auth_api/database/queries.py
@@ -1,6 +1,10 @@
 from lavoro_auth_api.database import db
+from lavoro_library.models import UserInDB
 
 
 def get_user_by_email(email: str):
     result = db.execute_query("SELECT * FROM account WHERE email = %s", (email,))
-    return result["result"]
+    if result["result"]:
+        return UserInDB(**result["result"][0])
+    else:
+        return None
--- lavoro_auth_api/routers/login.py
+++ lavoro_auth_api/routers/login.py
@@ -1,61 +1,16 @@
-import os
-
-from datetime import datetime, timedelta
-from typing import Annotated, Union
+from datetime import timedelta
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
-from lavoro_auth_api.database.queries import get_user_by_email
-from lavoro_library.models import User, UserInDB, Token, TokenData
+from lavoro_auth_api.helpers.login_helpers import authenticate_user, create_access_token
+from lavoro_library.models import Token
 
 router = APIRouter(prefix="/login", tags=["login"])
 
-SECRET_KEY = os.environ["SECRET_KEY"]
-ALGORITHM = "HS256"
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login/token")
-
-
-def verify_password(plain_password, password_hash):
-    return pwd_context.verify(plain_password, password_hash)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(email: str):
-    result = get_user_by_email(email)
-    if result:
-        user_dict = result[0]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(email: str, password: str):
-    user = get_user(email)
-    if not user:
-        return False
-    if not verify_password(password, user.password_hash):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 
 @router.post("/token", response_model=Token)
