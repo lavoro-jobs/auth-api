b95e8d33c5564afe4e2f15c63a7bd2558fe3661e
Jakov Vinožganić <jakov.vinozganic@fer.hr>
2023-10-30 16:00:53 +0100

    changed database names from singular to plural

--- auth-db.ddl.sql
+++ auth-db.ddl.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION "uuid-ossp";
 
 CREATE TYPE user_role AS ENUM ('applicant', 'recruiter');
 
-CREATE TABLE IF NOT EXISTS account (
+CREATE TABLE IF NOT EXISTS accounts (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS account (
     created_on TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS verification_token (
+CREATE TABLE IF NOT EXISTS verification_tokens (
     token VARCHAR(255) NOT NULL PRIMARY KEY UNIQUE,
-    account_id uuid NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+    account_id uuid NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
     expiry_date TIMESTAMP NOT NULL
 );
 
