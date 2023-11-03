abddd91ccafd96a5da2fda1e6b40ff124df9f707
Marko Bolt <marko.bolt@fer.hr>
2023-11-03 18:21:47 +0100

    modifications to registration endpoint

--- lavoro_auth_api/routers/register.py
+++ lavoro_auth_api/routers/register.py
@@ -1,48 +1,32 @@
+from enum import Enum
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import BaseModel, EmailStr, Field, SecretStr
+from pydantic import BaseModel, EmailStr, SecretStr
 
-from lavoro_auth_api.database.queries import (
-    get_user_by_email,
-    create_account,
-)
-from lavoro_auth_api.helpers.login_helpers import get_password_hash
-from lavoro_auth_api.helpers.register_helpers import create_confirmation_token
+from lavoro_auth_api.database.queries import get_user_by_email
+from lavoro_auth_api.helpers.register_helpers import  register_user
 
+class Role(str, Enum):
+    applicant = "applicant"
+    employer = "employer"
 
 class RegistrationForm(BaseModel):
-    email: EmailStr = Field(examples=['marko.horvat@gmail.com']) # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
-    password: SecretStr = Field(
-        json_schema_extra={
-            'title': 'Password', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
-            'description': 'Password of the user', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
-        }
-    )
-    role: str = Field(
-        examples=['applicant', 'recruiter'], # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
-        json_schema_extra={
-            'title': 'Role', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
-            'description': 'Role of the user', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
-        }
-    )
+    email: EmailStr
+    password: SecretStr
+    role: Role
 
 
 router = APIRouter(prefix="/register", tags=["register"])
 
 
-@router.post("/")
-def register_user(user: Annotated[RegistrationForm, Depends()]):
-    user_exists = get_user_by_email(user.email)
-    if user_exists:
+@router.post("")
+def register(form_data: Annotated[RegistrationForm, Depends()]):
+    user = get_user_by_email(form_data.email)
+    if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User already exists",
         )
-
-    hashed_password = get_password_hash(user.password.get_secret_value())
-    confirmation_token = create_confirmation_token()
-    affected_rows = create_account(
-        user.email, hashed_password, user.role, confirmation_token
-    )
-    # send_confirmation_email(username)
-    return {"message": "User successfuly created"}
+    confirmation_token = register_user(form_data.email, form_data.password.get_secret_value(), form_data.role)
+    # send_confirmation_email(form_data.email, confirmation_token) 
+    return {"message": f"Email sent to {form_data.email}"}
