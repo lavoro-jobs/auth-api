2abd17113ef8741434ec455c58c8ab14db2d1166
Marko Bolt <marko.bolt@fer.hr>
2023-11-03 22:57:37 +0100

    added sending confirmation emails

---
+++ lavoro_auth_api/helpers/email_helpers.py
@@ -0,0 +1,38 @@
+import os
+
+from fastapi_mail import FastMail, MessageSchema, MessageType, ConnectionConfig
+
+connection_config = ConnectionConfig(
+    MAIL_USERNAME = "lavoro.projektr@gmail.com",
+    MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD"),
+    MAIL_FROM="lavoro.projektr@gmail.com",
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_FROM_NAME="Lavoro",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
+)
+
+
+async def send_confirmation_email(email, token):
+    message_html = f"""
+    <html>
+        <body>
+            <h1>Confirm your email</h1>
+            <p>Please confirm your email by clicking on the link: <a href="http://localhost:8001/register/confirm/{token}">http://localhost:8001/register/confirm/{token}</a></p>
+        </body>
+    </html>
+    """
+
+    message = MessageSchema(
+        subject="Lavoro - Confirm your email",
+        recipients=[email],
+        body=message_html,
+        subtype=MessageType.html
+    )
+
+    fm = FastMail(connection_config)
+    await fm.send_message(message)
+    return True
--- lavoro_auth_api/routers/register.py
+++ lavoro_auth_api/routers/register.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, EmailStr, SecretStr
 
 from lavoro_auth_api.database.queries import get_user_by_email
 from lavoro_auth_api.helpers.register_helpers import register_user
+from lavoro_auth_api.helpers.email_helpers import send_confirmation_email
 
 
 class Role(str, Enum):
@@ -22,7 +23,7 @@ router = APIRouter(prefix="/register", tags=["register"])
 
 
 @router.post("")
-def register(form_data: Annotated[RegistrationForm, Depends()]):
+async def register(form_data: Annotated[RegistrationForm, Depends()]):
     user = get_user_by_email(form_data.email)
     if user:
         raise HTTPException(
@@ -30,5 +31,5 @@ def register(form_data: Annotated[RegistrationForm, Depends()]):
             detail="User already exists",
         )
     confirmation_token = register_user(form_data.email, form_data.password.get_secret_value(), form_data.role)
-    # send_confirmation_email(form_data.email, confirmation_token)
+    await send_confirmation_email(form_data.email, confirmation_token)
     return {"message": f"Email sent to {form_data.email}"}
