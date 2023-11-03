084919afade493e741521a3af42b3c0c5830b86c
Jakov Vinožganić <jakov.vinozganic@fer.hr>
2023-11-03 17:26:43 +0100

    Added RegistrationForm model instead of single parameters

--- lavoro_auth_api/routers/register.py
+++ lavoro_auth_api/routers/register.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException, status
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel, EmailStr, Field, SecretStr
 
 from lavoro_auth_api.database.queries import (
     get_user_by_email,
@@ -8,22 +10,39 @@ from lavoro_auth_api.helpers.login_helpers import get_password_hash
 from lavoro_auth_api.helpers.register_helpers import create_confirmation_token
 
 
+class RegistrationForm(BaseModel):
+    email: EmailStr = Field(examples=['marko.horvat@gmail.com']) # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
+    password: SecretStr = Field(
+        json_schema_extra={
+            'title': 'Password', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
+            'description': 'Password of the user', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
+        }
+    )
+    role: str = Field(
+        examples=['applicant', 'recruiter'], # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
+        json_schema_extra={
+            'title': 'Role', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
+            'description': 'Role of the user', # mozes maknut jer se to ne vidi na swaggeru ni nigdje (ili prepravit da se vidi)
+        }
+    )
+
+
 router = APIRouter(prefix="/register", tags=["register"])
 
 
 @router.post("/")
-def register_user(username: str, password: str, role: str):
-    user = get_user_by_email(username)
-    if user:
+def register_user(user: Annotated[RegistrationForm, Depends()]):
+    user_exists = get_user_by_email(user.email)
+    if user_exists:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User already exists",
         )
 
-    hashed_password = get_password_hash(password)
+    hashed_password = get_password_hash(user.password.get_secret_value())
     confirmation_token = create_confirmation_token()
     affected_rows = create_account(
-        username, hashed_password, role, confirmation_token
+        user.email, hashed_password, user.role, confirmation_token
     )
     # send_confirmation_email(username)
     return {"message": "User successfuly created"}
