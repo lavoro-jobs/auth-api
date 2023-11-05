1cb8540e0d171d84b861790e265aa7144fa0dabc
Jakov Vinožganić <jakov.vinozganic@fer.hr>
2023-11-05 16:38:43 +0100

    Added verification token trigger

--- auth-db.ddl.sql
+++ auth-db.ddl.sql
@@ -16,3 +16,15 @@ CREATE TABLE IF NOT EXISTS verification_tokens (
     account_id uuid NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
     expiry_date TIMESTAMP NOT NULL DEFAULT NOW() + INTERVAL '48 hours'
 );
+
+CREATE OR REPLACE FUNCTION delete_verification_token() RETURNS TRIGGER AS $$
+BEGIN
+    DELETE FROM verification_tokens WHERE account_id = OLD.id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_token 
+AFTER UPDATE ON accounts 
+FOR EACH ROW WHEN (OLD.is_active = FALSE AND NEW.is_active = TRUE)
+EXECUTE FUNCTION delete_verification_token();
